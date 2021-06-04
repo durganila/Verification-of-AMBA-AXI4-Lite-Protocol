89c486796fd4952d0e6050f8972c3076e473d488
manjarirpillai <manjarirpillai@gmail.com>
2021-06-04 03:51:46 -0700

    added few more tests

--- axi4-lite-interconnect/TestTypes/deterministic_tests.sv
+++ axi4-lite-interconnect/TestTypes/deterministic_tests.sv
@@ -10,84 +10,143 @@ class deterministic_tests extends generator;
 
     task execute();
 		int i,j;
-        /* //simple write and read txns to existing slave address
+		
+		 //write and read 1 byte data
+		driver_send(1'b1, 12'h4, 8'hEF, '0, '1);
+        #10;
+        driver_send(1'b1, 12'h4, '0, '1, '0);
+        #10; 
+		 
+		//write and read 2 bytes of data
+		driver_send(1'b1, 12'h4, 16'hBEEF, '0, '1);
+        #10;
+        driver_send(1'b1, 12'h4, '0, '1, '0);
+        #10;
+		
+		//write and read 3 bytes data
+		driver_send(1'b1, 12'h4, 24'hADBEEF, '0, '1);
+        #10;
+        driver_send(1'b1, 12'h4, '0, '1, '0);
+        #10;
+		
+        //simple write and read txns to existing slave address
 		driver_send(1'b1, 12'h4, 32'hDEADBEEF, '0, '1);
         #10;
         driver_send(1'b1, 12'h4, '0, '1, '0);
         #10; 
-		 */
-		/* //simple write and read txns to non-existing slave address
-		driver_send(1'b1, 16'h1003, 32'hFEEDBEEF, '0, '1);
-        #10;
-        driver_send(1'b1, 16'h1003, '0, '1, '0);
-        #10; */
 		
-		/* //write  and read address -'0 and 4095
+		//simple write and read txns to non-existing slave address
+		driver_send(1'b1, 12'hFFF, 32'hFEEDBEEF, '0, '1);
+        #10;
+        driver_send(1'b1, 12'hFFF, '0, '1, '0);
+        #10;
+		
+		//write  and read address -'0 and 2047
 		driver_send(1'b1, 12'h0, 32'h0FEDBEEF, '0, '1);
         #10;
         driver_send(1'b1, 12'h0, '0, '1, '0);
         #10;
-        driver_send(1'b1, 12'hFFF, '0, '0, '1);
+        driver_send(1'b1, 12'h7FF, '0, '0, '1);
         #10;
-        driver_send(1'b1, 32'hFFF, '0, '1, '0);
-        #10;  */ 
+        driver_send(1'b1, 32'h7FF, '0, '1, '0);
+        #10;  
 		
-		/* //write to locations from 0 to 2048 and read in reverse order
-		for(i = 0; i < 200; i=i+1) begin 
+		//write to locations from 0 to 2048 and read in reverse order
+		for(i = 0; i < 7FF; i=i+1) begin 
 			driver_send(1'b1, i, i+10, '0, '1);
 			#10;
 		end
-		for(i = 200; i >0; i=i-1) begin 
+		for(i = 7FF; i >=0; i=i-1) begin 
 			driver_send(1'b1, i, '0, '1, '0);
 			#10;
-		end */
+		end
 
-		/* //write  and read data - all zeros & all ones
+		//write  and read data - all zeros & all ones
 		driver_send(1'b1, 32'h4, '0, '0, '1);
         #10;
         driver_send(1'b1, 12'h4, '0, '1, '0);
         #10;
-        driver_send(1'b1, 32'h4, 8'hFF, '0, '1);
+        driver_send(1'b1, 32'h4, 32'hFFFFFFFF, '0, '1);
         #10;
         driver_send(1'b1, 32'h4, '0, '1, '0);
-        #10; */  
+        #10;  
 		
-		/* // consecutive writes (overwriting)  to same location followed by a read
-		driver_send(1'b1, 32'h4, 8'hBE, '0, '1);
+		// consecutive writes (overwriting)  to same location followed by a read
+		driver_send(1'b1, 32'h4, 32'hDEADBEEF, '0, '1);
         #10;
-		 driver_send(1'b1, 32'h4, 8'b10101110, '0, '1);
+		 driver_send(1'b1, 32'h4, 32'hFEEDBEEF, '0, '1);
         #10;
         driver_send(1'b1, 12'h4, '0, '1, '0);
-        #10; */
+        #10;
 		
 		/* //read and write to same location at same time
-		driver_send(1'b1, 32'h4, 8'h0F, '0, '1);
+		fork
+		driver_send(1'b1, 32'h4, 32'hFFFF, '0, '1);
         driver_send(1'b1, 32'h4, '0, '1, '0);
+		join
         #10; */
 		
 		/* //write to different locations at same time
-		driver_send(1'b1, 32'h4, 8'hBF, '0, '1);
-        driver_send(1'b1, 32'h14, 8'hAA, '0, '1);
+		driver_send(1'b1, 32'h4, 32'hFBFBF, '0, '1);
+        driver_send(1'b1, 32'h14, 32'hAAAAA, '0, '1);
         #10;
 		 */
-		/* //Modifying alternate locations and reading all the locations
-		for(j = 0; j < 4096; j=j+1) begin 
+		//Modifying alternate locations and reading all the locations
+		for(j = 0; j < 10; j=j+1) begin 
 			driver_send(1'b1, j, j+10, '0, '1);
 			#10;
 		end
-		for(j = 0; j < 4096; j=j+1) begin 
+		#10;
+		for(j = 0; j < 10; j=j+1) begin 
 			driver_send(1'b1, j, '0, '1, '0);
 			#10;
 		end
-		for(j = 0; j < 4096; j=j+10) begin 
+		#20;
+		for(j = 0; j < 10; j=j+2) begin 
 			driver_send(1'b1, j, '0, '0, '1);
 			#10;
 		end
-		for(j = 0; j < 4096; j=j+1) begin 
+		#10;
+		for(j = 0; j < 10; j=j+1) begin 
 			driver_send(1'b1, j, '0, '1, '0);
 			#10;
 		end 
-		 */
+		
+		/* //reset after write
+		driver_send(1'b1, 12'h4, 8'hEF, '0, '1);
+        #10;
+        driver_send(1'b0, '0, '0, '1, '0);
+        #10; 
+		 driver_send(1'b1, 12'h4, '0, '1, '0);
+        #10;  */
+		
+		//reset after read
+		driver_send(1'b1, 12'h4, 8'hEF, '0, '1);
+        #10;
+        driver_send(1'b1, 12'h4, '0, '1, '0);
+        #10;
+		driver_send(1'b0, '0, '0, '1, '0);
+        #10;
+		
+		//both read and write start signal asserted
+		driver_send(1'b1, 12'h4, 8'hEF, '1, '1);
+        #10;
+		
+		//neither read and write start signal asserted
+		driver_send(1'b1, 12'h4, 8'hEF, '0, '0);
+        #10;
+		
+		//read when reset asserted
+		driver_send(1'b1, 12'h4, 8'hEF, '0, '1);
+        #10;
+        driver_send(1'b0, 12'h4, '0, '1, '0);
+        #10; 
+		
+		//read when reset asserted
+		driver_send(1'b0, 12'h4, 8'hEF, '0, '1);
+        #10;
+		 driver_send(1'b1, 12'h4, '0, '1, '0);
+        #10;
 		
     endtask
 
